5704a9034de23dbf8e54eed05164b07c7bbae959
Tamas Piros <me@tamas.io>
2015-06-25 10:53:19 +0200

    update

--- films.js
+++ films.js
@@ -14,24 +14,8 @@ var findDocuments = function(db, callback) {
 
 var findOne = function(db, episodeNo, callback) {
   var episodeNo = Number(episodeNo);
-
-  if (episodeNo === 1) {
-    var title = 'Star Wars: Episode I - The Phantom Menace';
-  } else if (episodeNo === 2) {
-    var title = 'Star Wars: Episode II - Attack of the Clones';
-  } else if (episodeNo === 3) {
-    var title = 'Star Wars: Episode III - Revenge of the Sith';
-  } else if (episodeNo === 4) {
-    var title = 'Star Wars: Episode IV - A New Hope';
-  } else if (episodeNo === 5) {
-    var title = 'Star Wars: Episode V - The Empire Strikes Back';
-  } else {
-    var title = 'Star Wars: Episode VI - Return of the Jedi';
-  }
-
   var collection = db.collection('films');
-
-  collection.findOne({title: title}, {_id: 0}, function(err, doc) {
+  collection.findOne({episode: episodeNo}, {_id: 0}, function(err, doc) {
     callback(doc);
   });
 }
